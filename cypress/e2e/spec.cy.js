44472a27115ad8654833804aec5b7a19fa932e28
WBL-Training <training@whitebox-learning.com>
2024-06-06 05:25:29 +0530

    spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,8 +4,7 @@ describe("LinkedIn Job Application Form", () => {
       // Typing username and password
       cy.visit("https://www.linkedin.com/login");
       cy.get("#username").type("Enter email");
-      cy.get("#password").type("password").type("{enter}");
-
+      cy.get("#password").type("Enter Password {Enter}").wait(5000);
       // Waiting for login
       cy.wait(5000);
     });
@@ -29,102 +28,51 @@ describe("LinkedIn Job Application Form", () => {
     cy.get(
       ".search-reusables__filter-list > :nth-child(1) > .artdeco-pill"
     ).click();
-    cy.wait(3000);
 
-    // Clicking on "Easy Apply" in navigation bar
-    cy.contains("Easy Apply").first().click();
-    cy.wait(3000);
+    function easyApply() {
+      cy.wait(3000);
 
-    // cy.get('.jobs-search-results__list > li:first-child .jobs-apply-button--top-card').then(($button) => {
-    //   if ($button.hasClass('jobs-apply-button--applied')) {
-    //     // If the button has already been clicked, click on the next job application
-    //     cy.contains('.jobs-search-results__list > li', 'Easy Apply').eq(1).click();
-    //   } else {
-    //     // If the button has not been clicked, click on the first job application
-    //     $button.click();
-    //   }
-    // });
+      // Clicking on "Easy Apply" in navigation bar
+      cy.contains("Easy Apply").first().click();
+      cy.wait(3000);
 
-
-
-   
-    // Function to handle clicking on job applications until one without "Applied" is found
-    const applyJobs = () => {
+      // Handling easy apply process "RIGHT SIDE"
+      cy.wait(5000);
+      cy.get(".jobs-apply-button--top-card").first().click();
+      cy.wait(5000);
+      cy.get(".artdeco-text-input--input")
+        .click()
+        .clear()
+        .type("9876543210")
+        .wait(5000);
+      cy.get(".artdeco-button--primary").first().click();
+      cy.wait(6000);
       cy.get(
-        ".job-card-container__footer-item.job-card-container__footer-job-state"
+        ".artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view"
       )
-        .contains("Applied")
-        .then(($appliedJob) => {
-          if ($appliedJob.length > 0) {
-            cy.get(".display-flex.job-card-container.relative.job-card-list")
-              .eq(1)
-              .click();
-            applyJobs();
-          } else {
-            // No job application contains "Applied"
-            cy.log("All job applications applied");
-          }
-        });
-    };
+        .first()
+        .click();
 
-    // Clicking on the first job application
-    cy.get(
-      ".job-card-container__footer-item.job-card-container__footer-job-state"
-    )
-      .contains("Applied")
-      .then(($appliedJob) => {
-        if ($appliedJob.length > 0) {
-          // If the first job application contains "Applied", click on the second job application
-          cy.get(".display-flex.job-card-container.relative.job-card-list")
-            .eq(1)
-            .click();
-        } else {
-          // If the first job application does not contain "Applied", click on it
-          cy.get(
-            ".job-card-container__footer-item.job-card-container__footer-job-state"
-          )
-            .first()
-            .click();
-        }
-        // Apply for other jobs if needed
-        applyJobs();
-      });
+      cy.get(".artdeco-text-input--label").first();
 
+      cy.find("input").click().type("3");
 
+      cy.wait(3000);
 
+      cy.get('input[aria-required="true"]').click();
 
+      cy.wait(3000);
 
-    
-    // Handling easy apply process "RIGHT SIDE"
-    cy.wait(5000);
-    cy.get(".jobs-apply-button--top-card").first().click();
-    cy.wait(5000);
-    cy.get(".artdeco-text-input--input")
-      .click()
-      .clear()
-      .type("9876543210")
-      .wait(5000);
-    cy.get(".artdeco-button--primary").first().click();
-    cy.wait(6000);
-    cy.get(
-      ".artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view"
-    )
-      .first()
-      .click();
+      cy.contains("Review").click();
 
-    cy.get(".artdeco-text-input--label")
-      .invoke("text")
-      .then((text) => {
-        if (text.includes("*")) {
-          // If "*" is present, click on the input field and enter "3"
-          cy.get(".artdeco-text-input--label").click().type("3");
-        }
-      });
-    cy.contains("Review").click();
-
-    cy.get(".jobs-easy-apply-modal__content").scrollTo("bottom");
-    cy.wait(5000);
-    cy.contains("Submit").click();
-    cy.wait(5000);
+      cy.get(".jobs-easy-apply-modal__content").scrollTo("bottom");
+      cy.wait(5000);
+      cy.contains("Submit").click();
+      cy.wait(5000);
+      cy.get(".artdeco-modal__dismiss").click();
+      cy.wait(4000);
+      cy.get(".job-card-container__action").first().click();
+    }
+    easyApply();
   });
 });
