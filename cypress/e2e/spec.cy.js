776bb4555d20775f50809075c2622bbdd509402b
WBL-Training <training@whitebox-learning.com>
2024-06-08 05:37:48 +0530

    spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,21 +1,20 @@
 describe("LinkedIn Job Application Form", () => {
-  it("passes", () => {
+  before(() => {
     cy.session("login", () => {
-      // Typing username and password
       cy.visit("https://www.linkedin.com/login");
-      cy.get("#username").type("Enter email");
-      cy.get("#password").type("Enter Password {Enter}").wait(5000);
-      // Waiting for login
+      cy.get("#username").type("email@gmail.com");
+      cy.get("#password").type("Password {Enter}");
       cy.wait(5000);
     });
-    // Visiting LinkedIn homepage
+  });
+
+  it("applies for a job using Easy Apply", () => {
     cy.visit("https://www.linkedin.com");
     cy.wait(3000);
 
     // Initiating job search
-    cy.get(
-      ".global-nav__primary-items > :nth-child(2) > .app-aware-link > .t-12"
-    ).click();
+    cy.get(".global-nav__primary-items > :nth-child(2) > .app-aware-link > .t-12")
+      .click();
     cy.get(".search-global-typeahead__collapsed-search-button-text")
       .click()
       .wait(3000)
@@ -25,54 +24,47 @@ describe("LinkedIn Job Application Form", () => {
     cy.wait(3000);
 
     // Applying filters
-    cy.get(
-      ".search-reusables__filter-list > :nth-child(1) > .artdeco-pill"
-    ).click();
+    cy.get(".search-reusables__filter-list > :nth-child(1) > .artdeco-pill")
+      .click();
+    cy.wait(3000);
 
-    function easyApply() {
-      cy.wait(3000);
+    // Clicking on "Easy Apply"
+    cy.contains("Easy Apply").first().click();
+    cy.wait(5000);
 
-      // Clicking on "Easy Apply" in navigation bar
-      cy.contains("Easy Apply").first().click();
-      cy.wait(3000);
+    // Start the Easy Apply process
+    cy.get(".jobs-apply-button--top-card").first().click();
+    cy.wait(5000);
 
-      // Handling easy apply process "RIGHT SIDE"
-      cy.wait(5000);
-      cy.get(".jobs-apply-button--top-card").first().click();
-      cy.wait(5000);
-      cy.get(".artdeco-text-input--input")
-        .click()
-        .clear()
-        .type("9876543210")
-        .wait(5000);
-      cy.get(".artdeco-button--primary").first().click();
-      cy.wait(6000);
-      cy.get(
-        ".artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view"
-      )
-        .first()
-        .click();
+    cy.get('body').then(($body) => {
+      if ($body.find('.artdeco-text-input--input').length > 0) {
+        cy.get('.artdeco-text-input--input').first().click().wait(5000);
+      } else {
+        cy.get('.artdeco-text-input--input').eq(2).click().wait(5000);
+      }
+    });
 
-      cy.get(".artdeco-text-input--label").first();
+    cy.get(".artdeco-button--primary").first().scrollIntoView().click();
+    cy.wait(6000);
+    cy.get(".artdeco-button--primary").first().click();
+    
+    cy.get('.artdeco-text-input--input').each(($input) => {
+      cy.wrap($input).clear().type('4');
+    });
 
-      cy.find("input").click().type("3");
+    cy.wait(20000);
 
-      cy.wait(3000);
-
-      cy.get('input[aria-required="true"]').click();
-
-      cy.wait(3000);
-
-      cy.contains("Review").click();
-
-      cy.get(".jobs-easy-apply-modal__content").scrollTo("bottom");
-      cy.wait(5000);
-      cy.contains("Submit").click();
-      cy.wait(5000);
-      cy.get(".artdeco-modal__dismiss").click();
-      cy.wait(4000);
-      cy.get(".job-card-container__action").first().click();
-    }
-    easyApply();
+    cy.get('button:contains("Review"), button:contains("Next")')
+      .first()
+      .click()
+      .then(() => {
+        cy.get(".jobs-easy-apply-modal__content").scrollTo("bottom");
+        cy.wait(5000);
+        cy.contains("Submit").click();
+        cy.wait(5000);
+        cy.get(".artdeco-modal__dismiss").click();
+        cy.wait(4000);
+        cy.get(".job-card-container__action").first().click();
+      });
   });
 });
